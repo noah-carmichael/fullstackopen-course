27e4101a6cb63bfb41026b8f965da6e6866a6c6d
Noah Carmichael <1noah.carmichael@gmail.com>
2024-12-25 01:57:20 -0500

    Part 1a initial

---
+++ part1/introdemo/src/App.jsx
@@ -0,0 +1,88 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log(now, a+b)
+//   return (
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+// const Hello = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <p>Hello {props.name}, you are {props.age} years old</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name='Maya' age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App
